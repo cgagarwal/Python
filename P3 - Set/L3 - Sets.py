10e302d6541d0ec687a61c1b106fa8dee7a8c769
Chirag Goyal <78220020+cgagarwal@users.noreply.github.com>
2021-05-13 20:36:31 +0530

    Create L3 - Sets.py

---
+++ P3 - Set/L3 - Sets.py
@@ -0,0 +1,10 @@
+# Sets are immmutable
+# class <set>
+''' They are basically keys of dictionary '''
+''' They are randomly arranged so no indexing '''
+
+s = set()       # initializing
+s = {1,2,4,'gr'}
+
+s.add(12)       # add the element
+s.remove('gr')    # delete the element
