559e2df482b37ce597594680b9214aff10665798
Chirag Goyal <78220020+cgagarwal@users.noreply.github.com>
2021-05-13 11:26:05 +0530

    Create L2 - Operator.py

---
+++ P1 - Basics/L2 - Operator.py
@@ -0,0 +1,26 @@
+# Arithmetical Operators
+
+print(3+4)    # 7
+print(5-2)    # 3
+print(5*2)    # 10
+print(5**2)   # 25
+print(4/2)    # 2.0 (float)
+print(5//2)   # 2 (int) ; quotient
+print(5%2)    # 1       ; remainder
+
+# Relational Operators
+
+print(4 == 6, 5 != 5)        # False
+print(4 > 6, 4 >= 6)         # True
+print(5 < 12, 5 <= 12)       # True
+
+
+# Assignment Operators
+
+x = 8         # 8
+x += 6        # 14
+x -= 2        # 12
+x //= 5       # 2
+x **= 4       # 16
+x *= 2        # 32
+x /= 4        # 8.0
