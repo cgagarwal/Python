a60d589d1f4436fa8b2235268e2a5f25c82ce337
Chirag Goyal <78220020+cgagarwal@users.noreply.github.com>
2021-05-13 15:53:55 +0530

    Create L5 - Operations.py

---
+++ P1 - Basics/L5 - Operations.py
@@ -0,0 +1,25 @@
+# Bitwise Operator
+''' convert number in binary ; do bitwise operation
+    then again converts in decinmal '''
+
+print(a & b)            # AND gate
+print(a | b)            # OR gate
+print(~a)               # NOT gate
+print(a^b)              # XOR gate
+
+print(a<<2)             # append 2 zero at last  (left shift)
+print(a>>2)             # remove 2 digits from last  (right shift)
+
+
+# Membership Operators
+
+print(a in list)          # True
+print(a not in list)      # False
+
+
+# Identity Operator
+''' Both the values are of same type and same value
+              True/False          '''
+
+print((2==2) is True)     # True
+print(4 is True)           # False
