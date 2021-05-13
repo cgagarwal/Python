2a4bf3c38a359a9676497c977f32089018165c13
Chirag Goyal <78220020+cgagarwal@users.noreply.github.com>
2021-05-13 11:15:38 +0530

    Create L1 - Print.py

---
+++ P1 - Basics/L1 - Print.py
@@ -0,0 +1,26 @@
+print('string')
+print("str")
+print('''multiline string 
+is cool by triple quotes''')
+
+
+'''print(type(x)) is a fn
+  (multiline comment) ''' # use for checking class 
+
+x = 0 , -2 , 8 , 12       # class <int>
+x = 56.4 , -34.2 , 2.0    # class <float>
+x = True , False          # class <bool>
+x = 'str' , ''string''    # class <str>
+x = 5j , -1+4j , 0j       # class <complex>
+x = None                  # class <None>
+
+# , is used for space inside print
+print('\n')  # new line
+print("'","\'",'\'') # '
+print('"','\"',"\"") # "
+print('\t')  # tab space
+print('\\')  # back slash  \
+print('\\\\\\')  # \\\
+
+
+# input() for taking input
