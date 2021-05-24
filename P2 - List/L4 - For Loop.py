250fc92375afb1775becdf51d668ee8f326549fd
Chirag Goyal <78220020+cgagarwal@users.noreply.github.com>
2021-05-24 11:09:26 +0530

    Create L4 - For Loop.py

---
+++ P2 - List/L4 - For Loop.py
@@ -0,0 +1,22 @@
+# It access set , list , tuple easily
+
+for i in set:
+  print(i , end = ' ')      # elements are printed by space
+  
+for i in range(start , end + 1 , jump):
+  pass
+
+for i in range(0,3):
+  print(i)         # 0 , 1 , 2
+  
+for i in range(4):
+  # 0 , 1 , 2 , 3
+
+for i in range(0,8, 2):
+  #   0 , 2 , 4 , 6
+  
+for i in range(5 , -1 , -1):
+  print(i)
+  
+for i in reversed(range(3)):
+  # 2 , 1 , 0
