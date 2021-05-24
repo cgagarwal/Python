b178daf351979b9f5d68908d94b730fed7206565
Chirag Goyal <78220020+cgagarwal@users.noreply.github.com>
2021-05-24 19:30:28 +0530

    Update L4 - Inbuilt.py

--- P4 - Function/L4 - Inbuilt.py
+++ P4 - Function/L4 - Inbuilt.py
@@ -11,3 +11,11 @@ x , y = float('inf') , - float('inf')     # infinity
 print("%0.6f"%num)              # value after 6 precedding digit of decimal
 
 li = list(map(int , input().split()))     # for multiple input through mapping
+
+from itertools import product         # Cartesian product
+arr = [1,2]
+print(list(product(arr,arr)))           # [(1, 1), (1, 2), (2, 1), (2, 2)]
+print(list(product(arr,repeat = 3)))
+
+A = [[1,2],[1,2,3]]
+print(list(product(*A)))        #  [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3)]
