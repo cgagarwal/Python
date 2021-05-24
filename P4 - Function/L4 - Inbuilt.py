65e242586f01a512f57062e2ea06f7a12ab2b8de
Chirag Goyal <78220020+cgagarwal@users.noreply.github.com>
2021-05-24 19:11:57 +0530

    Create L4 - Inbuilt.py

---
+++ P4 - Function/L4 - Inbuilt.py
@@ -0,0 +1,13 @@
+from random import randrange
+a = randrange(start,end)    # takes random value from start to end
+
+print(abs(-4))                  # 4   absolute value
+print(isinstance(10 , int))      # True 
+
+import sys
+a = sys.maxsize                         # max element in python
+x , y = float('inf') , - float('inf')     # infinity
+
+print("%0.6f"%num)              # value after 6 precedding digit of decimal
+
+li = list(map(int , input().split()))     # for multiple input through mapping
