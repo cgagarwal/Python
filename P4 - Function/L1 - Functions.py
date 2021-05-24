40fd89f591bc0eba35de397fa979feac886b490d
Chirag Goyal <78220020+cgagarwal@users.noreply.github.com>
2021-05-24 10:31:19 +0530

    Create L1 - Functions.py

---
+++ P4 - Function/L1 - Functions.py
@@ -0,0 +1,26 @@
+def function_name(arg):
+  ''' Documentary of the function
+  This is yours docstring   '''
+  return                  # default return None
+
+# argument , return are not compulsory
+
+print(function_name.__doc__)       # Documentay of function is printed
+
+fun()           # execute the function
+x = fun()       # execute and return assigned to x
+print(fun())      # execute and print return
+
+# Recursive
+
+def fn():
+  # base case
+  # self work and again call function
+  # call stacks on Top (local variable)
+  
+ # Scope of Variable
+
+'''
+Local Variable      =  only use in function
+Global Variable     =  Use everywhere
+'''
