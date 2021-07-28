ef2de691176b0c034ed1a0582da65ac9b0efee8f
Chirag Goyal <78220020+cgagarwal@users.noreply.github.com>
2021-07-28 18:13:18 +0530

    Update L3 - Sets.py

--- P3 - Set/L3 - Sets.py
+++ P3 - Set/L3 - Sets.py
@@ -8,3 +8,9 @@ s = {1,2,4,'gr'}
 
 s.add(12)       # add the element
 s.remove('gr')    # delete the element
+s.discard(56)    # work like remove --- does not give error for invalid element
+s.pop()         # remove element randomly
+
+# take input of set in single line by mapping
+S = set(map(int , input().split()))
+# also by usinf for loop
